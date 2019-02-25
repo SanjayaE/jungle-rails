32ecc690e6276abf37b52bb8caefe981531ea4f4
SanjayaE <sanjaya4u@gmail.com>
2019-02-25 02:24:33 +0000

    form created to allow users to post reviews- tested form is working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,12 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
-    puts @products[0].inspect
+    # puts @products[0].inspect
   end
 
   def show
     @product = Product.find params[:id]
+    @review = Review.new
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
  # this will create route setup for products with (limited to index and show)
-  resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
